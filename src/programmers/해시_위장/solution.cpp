97ab8e86c7af727fbe4b38e3624af28948ea313c
HyunA-Kim <65345381+kha-github@users.noreply.github.com>
2020-09-26 02:59:32 +0900

    update 해시_위장

--- src/programmers/해시_위장/solution.cpp
+++ src/programmers/해시_위장/solution.cpp
@@ -1,24 +1,18 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <algorithm>
-#include <string>
 
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
     unordered_map<string, vector<string>> hash_map;
-    vector<string> type;
-
-    for (int i = 0; i< clothes.size(); i++){
+    
+    for (int i = 0; i< clothes.size(); i++)
         hash_map[clothes[i][1]].push_back(clothes[i][0]);
-        if (find(type.begin(), type.end(), clothes[i][1]) == type.end())
-            type.push_back(clothes[i][1]);
-    }
-
-    for (int i = 0; i < type.size(); i++)
-        answer *= (hash_map[type[i]].size()+1);
-
+    
+    for (auto iter = hash_map.begin(); iter != hash_map.end(); iter++)
+        answer *= (iter->second.size()+1);
+    
     return answer-1;
 }
