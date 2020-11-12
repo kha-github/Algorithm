e6ceb6884c31aeed464e157cacf4c3cbf215b8a7
HyunA-Kim <65345381+kha-github@users.noreply.github.com>
2020-11-13 00:40:33 +0900

    Create solution.cpp

---
+++ src/baekjoon/DP/9095(1,2,3 더하기)/solution.cpp
@@ -0,0 +1,44 @@
+#include <string.h>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+#define MAXIMUM 1000000
+using namespace std;
+
+int funct(int number);
+int dptable[12];
+
+int main() {
+	vector<int> vec;
+
+	int count;
+	int number;
+	cin >> count;
+	
+	for (int i = 0; i < count; i++) {
+		cin >> number;
+		vec.push_back(number);
+	}
+
+	for (int i = 0; i < count; i++) {
+		cout << funct(vec[i]) << endl;
+		memset(dptable, 0, 12);
+	}
+
+	return 0;
+}
+
+int funct(int number) {
+	
+	if (number < 0) return 0;
+	if (number == 0) return 1;
+	if (dptable[number] > 0) return dptable[number];
+
+	dptable[number] = funct(number - 1) + funct(number - 2) + funct(number - 3);
+
+	return dptable[number];
+
+}
