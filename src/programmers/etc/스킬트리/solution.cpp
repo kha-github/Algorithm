814a37ff42b4612d7033724ebc4a58727e14c186
HyunA-Kim <65345381+kha-github@users.noreply.github.com>
2020-12-14 15:08:04 +0900

    Create solution.cpp

---
+++ src/programmers/etc/스킬트리/solution.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) {
+    int answer = 0;
+    
+    int size = skill_trees.size();
+    
+    for (int i = 0; i < size ; i++){
+        string copy_skill = skill;
+        answer++;
+        
+        int len = skill_trees[i].length();
+        
+        for (int k = 0; k < len; k++){
+             if (copy_skill.find(skill_trees[i][0]) != std::string::npos) {
+
+                cout << skill_trees[i][0] << copy_skill[0] << endl;
+                if (skill_trees[i][0] == copy_skill[0]) {
+                    skill_trees[i].erase(0,1);
+                    copy_skill.erase(0, 1);                 
+                }
+                else {
+                    answer--;
+                    cout << answer << endl;
+                    break;
+                }
+            }
+            else{
+                skill_trees[i].erase(0,1);
+            }
+        }
+    }
+    
+    return answer;
+}
