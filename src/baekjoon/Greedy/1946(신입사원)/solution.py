e5e8309b6c195384cdbb9c7bdc1e34c13fc89699
HyunA-Kim (Mina) <65345381+kha-github@users.noreply.github.com>
2021-03-25 02:30:30 +0900

    2021.03.25

--- src/baekjoon/Greedy/1946(신입사원)/solution.py
+++ src/baekjoon/Greedy/1946(신입사원)/solution.py
@@ -1,1 +1,19 @@
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  mylist=[]
+  for _ in range(n):
+    mylist.append(list(map(int, input().split())))
 
+  mylist.sort(key=lambda x:x[0])
+
+  mylist = list(zip(*mylist))[1]
+  mymin = mylist[0]
+
+  for i in range(1, n):
+    if mylist[i] > mymin:
+      n -= 1
+    else:
+      mymin = mylist[i]
+  
+  print(n)
